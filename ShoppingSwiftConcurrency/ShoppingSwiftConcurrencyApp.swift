41232dc5d5d8b2cf7089c879b0e0f80b4df95f45
仲優樹 <yu-ki216m@outlook.jp>
2025-06-28 07:24:24 +0900

    Initial Commit

---
+++ ShoppingSwiftConcurrency/ShoppingSwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingSwiftConcurrencyApp.swift
+//  ShoppingSwiftConcurrency
+//
+//  Created by 仲優樹 on 2025/06/28.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingSwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
